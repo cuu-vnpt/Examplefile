a84434aacf028ca8363b47afd0722bcb3073d495
cuu-vnpt <kunnobita97@gmail.com>
2024-03-05 16:53:15 +0700

    newfile

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+{
+    string sourcepath = @"/Users/macos/Desktop/logo/DATA/Map.txt";
+
+        StreamReader reader = new StreamReader(sourcepath);
+        string line = "";
+        int sum=0;
+        while ((line=reader.ReadLine()) !=null)
+        {
+            Console.WriteLine(line);
+            sum+=Int32.Parse(line);
+        }
+        reader.Close();
+        Console.WriteLine(sum);
+}
+}
